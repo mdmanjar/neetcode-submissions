e2c1602e6cb592f9319986d9fa8b773ed43af998
mdmanjar <mdmanjar7929@gmail.com>
2026-09-23 18:59:48 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        @lru_cache(None)
+        def dfs(i,target):
+            if i==len(nums):
+                return 1 if target==0 else 0
+            return dfs(i+1,target-nums[i])+dfs(i+1,target+nums[i])
+        return dfs(0,target)
+        
